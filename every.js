f91d0700a8ac71ebf4d3c401d934ae27b4e6d9e2
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2016-09-01 19:25:32 -0600

    all tests pass

--- every.js
+++ every.js
@@ -9,14 +9,18 @@ module.exports = {
 // are even numbers.
 
 function allEven (input) {
-  return input;
+  return input.every(function(element) {
+    return (element % 2 == 0);
+  });
 };
 
 // Check to see if all elements in an array
 // are of the same type.
 
 function allSameType (input) {
-  return input;
+  return input.every(function(element) {
+    return (typeof input[0] === typeof element);
+  });
 };
 
 // Check to see if every element in the matrix is
@@ -24,12 +28,39 @@ function allSameType (input) {
 // greater than 0.
 
 function positiveMatrix (input) {
-  return input;
+  return input.every(function(element) {
+    if ( element.constructor !=  Array) {
+      return false;
+    }
+    return element.every(function(number) {
+      return number > 0;
+    })
+  });
 };
 
 // Check that all items in an array are strings
 // and that they all only contain the same vowels.
 
 function allSameVowels (input) {
-  return input;
+
+  return input.every(function(element) {
+
+    if (element.constructor == String) {
+
+        let hasVowel = false;
+
+        for (let i of ['a', 'e', 'i', 'o', 'u']) {
+          if (element.includes(i)) {
+            if (hasVowel == true){
+              return false
+            } else {
+              hasVowel = true;
+            }
+          }
+        }
+
+        return true;
+
+    }
+  });
 };
--- some.js
+++ some.js
@@ -5,30 +5,40 @@ module.exports = {
   lostCarcosa : lostCarcosa
 };
 
-// Check to see if any of the elements in the 
+// Check to see if any of the elements in the
 // array are numbers greater than 10.
 
 function anyGreaterThan10 (input) {
-  return input;
+  return input.some(function(element) {
+    return element > 10
+  });
 };
 
 // Check to see if any of the strings in
 // the array is longer than 10 characters.
 
 function longWord (input) {
-  return input;
+  return input.some(function(element) {
+    return element.length > 10
+  });
 };
 
 // Check to see if any of the elements in
 // the matrix are true.
 
 function truePossibilities (input) {
-  return input;
+  return input.some(function(array) {
+    return array.some(function(element) {
+      return element;
+    });
+  });
 };
 
 // Check to see if 'Lost' is in
 // the phrase (using some).
 
 function lostCarcosa (input) {
-  return input;
+  return input.some(function(element) {
+    return element.includes('Lost');
+  });
 };
